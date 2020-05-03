ea8d17a70165b21b47331d54d35723b7a5073821
garayj <garayj@oregonstat.edu>
2020-05-03 12:53:38 -0700

    Completed python task.'

---
+++ Python/mypython.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+# Only print lowercase strings.
+letters = string.ascii_lowercase
+
+for i in range(3):
+    # Create a string and print it.
+    randomString = ''.join(random.choice(letters) for i in range(10))
+    print(randomString)
+
+    # Open file to write to with the same name as the string
+    f = open(randomString + ".txt", "w")
+    f.write(randomString + "\n")
+    f.close()
+
+randMulti = 1
+for i in range(2):
+    # Create a new number.
+    randNum = random.randint(1, 43)
+    # Multuply and print
+    randMulti = randMulti * randNum
+    print(randNum)
+
+print(randMulti)
