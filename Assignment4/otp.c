b2147b057582be1a381a3e5eecb7ccf90f75c9ca
Jose <garayj@oregonstate.edu>
2020-06-01 17:54:27 -0700

    removing testing files and renaming files.

--- Assignment4/otp.c
+++ Assignment4/otp.c
@@ -102,36 +102,90 @@ void decrypt()
     }
     printf("%s\n", decryptedText);
 }
-int sendAll(int s, char *buf, int *len)
+
+int sendAll(int s, char *buf)
 {
+    int len = strlen(buf);
     int total = 0;
-    int bytesLeft = *len;
+    int bytesLeft = len;
     int n;
     int counter = 0;
-    while (total < *len)
+    while (total < len)
     {
-        printf("%d\n", counter);
         counter++;
-        n = send(s, buf + total, bytesLeft, 0);
+        n = send(s, buf + total, 1000, 0);
         if (n == -1)
             break;
         total += n;
         bytesLeft -= n;
     }
-    *len = total;
     return n == -1 ? -1 : 0;
+}
+
+int setupAndConnectSocket(int portNumber)
+{
+    struct sockaddr_in serverAddress;
+    struct hostent *serverHostInfo;
+    // Set up the server address struct
+    memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+    serverAddress.sin_family = AF_INET;                          // Create a network-capable socket
+    serverAddress.sin_port = htons(portNumber);                  // Store the port number
+    serverHostInfo = gethostbyname("localhost");                 // Convert the machine name into a special form of address
+    if (serverHostInfo == NULL)
+    {
+        fprintf(stderr, "CLIENT: ERROR, no such host\n");
+        exit(1);
+    }
+    memcpy((char *)&serverAddress.sin_addr.s_addr, (char *)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+    // Set up the socket
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+    if (socketFD < 0)
+        error("CLIENT: ERROR opening socket");
+    // Connect to server
+    if (connect(socketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+    {
+        error("CLIENT: ERROR connecting");
+    }
+    return socketFD;
+}
+void receive(int socketFD)
+{
+    char buffer[1001];
+    int charsRead, length, counter;
+    memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+
+    // Receive the length of the incoming
+    charsRead = recv(socketFD, buffer, 1000, 0); // Read data from the socket, leaving \0 at end
+    if (charsRead < 0)
+        error("CLIENT: ERROR reading from socket");
+
+    length = atoi(buffer);
+    counter = 0;
+    ciphertext = calloc(length + 1, sizeof(char));
+
+    while (length > counter)
+    {
+        memset(buffer, '\0', sizeof(buffer));        // Clear out the buffer again for reuse
+        charsRead = recv(socketFD, buffer, 1000, 0); // Read data from the socket, leaving \0 at end
+        if (charsRead < 0)
+            error("CLIENT: ERROR reading from socket");
+        strcat(ciphertext, buffer);
+        counter += strlen(ciphertext);
+    }
+    if (strcmp(ciphertext, "1") == 0)
+        error("User has no messages");
 }
 
 int main(int argc, char *argv[])
 {
+    // Setup most variables that are needed.
     int socketFD, portNumber, charsWritten, charsRead;
-    struct sockaddr_in serverAddress;
-    struct hostent *serverHostInfo;
-    char buffer[256];
     char *user, *messageFileName, *keyFileName, *command;
 
     bool isValidGet = (argc == 5 && strcmp("get", argv[1]) == 0) ? true : false;
     bool isValidPost = (argc == 6 && strcmp("post", argv[1]) == 0) ? true : false;
+
     // Check usage & args
     if (!isValidGet && !isValidPost)
     {
@@ -169,63 +223,48 @@ int main(int argc, char *argv[])
     if (isValidGet)
     {
         keyFileName = argv[3];
+        // Get the contents of the key.
         openAndStoreContents(keyFileName, &key);
-        openAndStoreContents("jose.4370", &ciphertext);
-        decrypt();
     }
 
-    // Set up the server address struct
-    memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-    serverAddress.sin_family = AF_INET;                          // Create a network-capable socket
-    serverAddress.sin_port = htons(portNumber);                  // Store the port number
-    serverHostInfo = gethostbyname("localhost");                 // Convert the machine name into a special form of address
-    if (serverHostInfo == NULL)
-    {
-        fprintf(stderr, "CLIENT: ERROR, no such host\n");
-        exit(1);
-    }
-    memcpy((char *)&serverAddress.sin_addr.s_addr, (char *)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+    // Set up the connection to the server.
+    socketFD = setupAndConnectSocket(portNumber);
 
-    // Set up the socket
-    socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-    if (socketFD < 0)
-        error("CLIENT: ERROR opening socket");
-
-    // Connect to server
-    if (connect(socketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
-    {
-        error("CLIENT: ERROR connecting");
-    }
-
+    // Create the payload to be sent to the server.
+    // Calculate the length of the payload that will be sent to the server.
+    // A payload will always include the command and the user name. If the
+    // command is post, include the cipher text as well.
     int payloadLength = strlen(command) + 2 + strlen(user);
     if (isValidPost)
-    {
-        payloadLength += strlen(ciphertext);
-        payloadLength += 1;
-    }
+        payloadLength += strlen(ciphertext) + 1;
 
+    // Create an array big enough for the payload.
     char payload[payloadLength];
     memset(payload, '\0', sizeof(payload));
+
+    // Format the payload where each block of information that is send is delimited by a space.
     if (isValidPost)
     {
+        // Post sends the command, user, length of the cipher text, and the cipher text.
         int ciphertextLength = strlen(ciphertext);
         sprintf(payload, "%s %s %d %s", command, user, ciphertextLength, ciphertext);
     }
     else
+        // Get sends command and user.
         sprintf(payload, "%s %s", command, user);
-    int paylen = strlen(payload);
 
-    // Send message to server
-    charsWritten = sendAll(socketFD, payload, &paylen); // Write to the server
+    // For sending data to the server for both get and post.
+    // Send message to server. Adapted sendAll from the beej networking guide.
+    charsWritten = sendAll(socketFD, payload); // Write to the server
     if (charsWritten < 0)
         error("CLIENT: ERROR writing to socket");
 
-    // // Get return message from server
-    // memset(payload, '\0', sizeof(payload));                  // Clear out the buffer again for reuse
-    // charsRead = recv(socketFD, payload, sizeof(payload), 0); // Read data from the socket, leaving \0 at end
-    // if (charsRead < 0)
-    //     error("CLIENT: ERROR reading from socket");
-    // printf("CLIENT: I received this from the server: \"%s\"\n", payload);
+    // Get return message from server
+    if (isValidGet)
+    {
+        receive(socketFD);
+        decrypt();
+    }
 
     close(socketFD); // Close the socket
     freeMemory();
---
+++ Assignment4/otp_d.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <limits.h>
+
+// Boolean definition.
+typedef enum
+{
+    false,
+    true
+} bool;
+
+int childStatus = -99, children = 0;
+struct sigaction action_SIGCHILD = {0};
+char *ciphertext = NULL;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+} // Error function used for reporting issues
+void CatchSIGCHLD(int signo)
+{
+    if (children > 0)
+        children--;
+}
+
+void getOldestFile(char *user, char *fileName)
+{
+    struct dirent *de; // Pointer for directory entry
+    DIR *dr = opendir(".");
+    int oldestTimeStamp = -1;
+    struct stat sb;
+    // Read the directory.
+    while ((de = readdir(dr)) != NULL)
+    {
+        // Look for files that have my OSU ID and ".rooms" in them.
+        if (strstr(de->d_name, user))
+        {
+            // Store stats in the stat buffer.
+            stat(de->d_name, &sb);
+            // If this is the first time running, it's the most recent.
+            if (oldestTimeStamp == -1 || oldestTimeStamp > (int)sb.st_mtime)
+            {
+                oldestTimeStamp = (int)sb.st_mtime;
+                strcpy(fileName, de->d_name);
+            }
+        }
+    }
+    closedir(dr);
+}
+
+void openAndStoreContents(char *fileName, char **store)
+{
+    char *line = NULL;
+    size_t len = 0;
+    FILE *file;
+
+    file = fopen(fileName, "r");
+    // Check for errors.
+    if (file == NULL)
+    {
+        return;
+    }
+    getline(&line, &len, file);
+    line[strcspn(line, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+    *store = line;
+    fclose(file);
+}
+
+int sendAll(int s, char *buf)
+{
+    int len = strlen(buf);
+    int total = 0;
+    int bytesLeft = len;
+    int n;
+    int counter = 0;
+    while (total < len)
+    {
+        counter++;
+        n = send(s, buf + total, 1000, 0);
+        if (n == -1)
+            break;
+        total += n;
+        bytesLeft -= n;
+    }
+    return n == -1 ? -1 : 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+    socklen_t sizeOfClientInfo;
+    char buffer[1001];
+    struct sockaddr_in serverAddress, clientAddress;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "USAGE: %s port\n", argv[0]);
+        exit(1);
+    } // Check usage & args
+
+    // Signal handler for SIGCHILD
+    action_SIGCHILD.sa_handler = CatchSIGCHLD;
+    action_SIGCHILD.sa_flags = SA_RESTART;
+    sigfillset(&action_SIGCHILD.sa_mask);
+    sigaction(SIGCHLD, &action_SIGCHILD, NULL);
+
+    // Set up the address struct for this process (the server)
+    memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+    portNumber = atoi(argv[1]);                                  // Get the port number, convert to an integer from a string
+    serverAddress.sin_family = AF_INET;                          // Create a network-capable socket
+    serverAddress.sin_port = htons(portNumber);                  // Store the port number
+    serverAddress.sin_addr.s_addr = INADDR_ANY;                  // Any address is allowed for connection to this process
+
+    // Set up the socket
+    listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+    if (listenSocketFD < 0)
+        error("ERROR opening socket");
+
+    // Enable the socket to begin listening
+    if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+        error("ERROR on binding");
+    listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+    while (1)
+    {
+        // Accept a connection, blocking if one is not available until one connects
+        sizeOfClientInfo = sizeof(clientAddress);                                                               // Get the size of the address for the client that will connect
+        establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+        if (establishedConnectionFD < 0)
+            error("ERROR on accept");
+
+        // Check if there are too many children.
+        if (children > 5)
+        {
+            close(establishedConnectionFD);
+            printf("Maximum connections. Try again later\n");
+            fflush(stdin);
+            continue;
+        }
+
+        // Create a forked process.
+        pid_t childThread = fork();
+
+        // If the creation of the thread is corrupted somehow,
+        // shoot out an error.
+        if (childThread == -1)
+        {
+            perror("Fork is bork!");
+            exit(1);
+        }
+
+        // Child process path.
+        if (childThread == 0)
+        {
+            // Immediately call sleep per the requirements.
+            sleep(2);
+            // Increment the number of children used
+            children++;
+            close(listenSocketFD); // Close the listening socket
+
+            // Setup vavriables
+            char *saveptr, command[5], user[50], *length;
+            memset(user, '\0', sizeof(user));
+            memset(command, '\0', sizeof(command));
+            int messageLength, counter = 0,
+                               pidInt = (int)getpid(),
+                               pidLength = snprintf(NULL, 0, "%d", pidInt);
+
+            // Get the message from the client and display it
+            memset(buffer, '\0', sizeof(buffer));
+            charsRead = recv(establishedConnectionFD, buffer, 1000, 0); // Read the client's message from the socket
+            if (charsRead < 0)
+                error("ERROR reading from socket");
+
+            // Get the command and the user.
+            strcpy(command, strtok_r(buffer, " ", &saveptr));
+            strcpy(user, strtok_r(NULL, " ", &saveptr));
+
+            // POST: Receives username and message via socket. Write the message to a file and
+            // print path to file (Relative?). Should crash or terminate, still should be able to read files.
+            if (strcmp(command, "post") == 0)
+            {
+                // Get the length of the payload and convert it to an integer.
+                length = strtok_r(NULL, " ", &saveptr);
+                messageLength = atoi(length);
+
+                // Create a string with enough space for the message and copy it in from the buffer.
+                char message[messageLength + 1];
+                memset(message, '\0', sizeof(message));
+                strcpy(message, strtok_r(NULL, "\0", &saveptr));
+                // Keep track of the number of characters received.
+                counter += strlen(message);
+                // Loop until the entire message is received if it wasn't received in the first packet that was sent.
+                while (messageLength > counter)
+                {
+                    // Receive another thing of stuff.
+                    memset(buffer, '\0', sizeof(buffer));
+                    charsRead = recv(establishedConnectionFD, buffer, 1000, 0); // Read the client's message from the socket
+                    if (charsRead < 0)
+                        error("ERROR reading from socket");
+
+                    // Concat that just came in with the message that we already have and increment the counter.
+                    strcat(message, buffer);
+                    counter += charsRead;
+                }
+
+                // Write the encrypted message to a file with the name of "[user].[pid]".
+                // Format the file name.
+                char fileName[strlen(user) + pidLength + 1];
+                memset(fileName, '\0', sizeof(fileName));
+                sprintf(fileName, "%s.%d", user, pidInt);
+
+                // Format the message. Add the newline character to the end.
+                char formattedMessage[strlen(message) + 2];
+                memset(formattedMessage, '\0', sizeof(formattedMessage));
+                sprintf(formattedMessage, "%s\n", message);
+
+                // Open the file, check if the file was actually opened, and then write the message in it.
+                int file_descriptor = open(fileName, O_WRONLY | O_CREAT, 0600);
+                if (file_descriptor < 0)
+                    error("File could not be opened.");
+                write(file_descriptor, formattedMessage, strlen(formattedMessage) * sizeof(char));
+                close(file_descriptor);
+
+                // Get the current working directory, and then append the file that was just written.
+                // Source: https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
+                char cwd[PATH_MAX];
+                memset(cwd, '\0', sizeof(cwd));
+                if (getcwd(cwd, sizeof(cwd)) == NULL)
+                {
+                    perror("getcwd() error");
+                    exit(1);
+                }
+                sprintf(cwd, "%s/%s", cwd, fileName);
+                printf("%s\n", cwd);
+                fflush(stdin);
+            }
+            if (strcmp(command, "get") == 0)
+            {
+                char fileName[256];
+                memset(fileName, '\0', sizeof(fileName));
+                getOldestFile(user, fileName);
+                // Read the file
+                // Store the contents of the file in a string.
+                openAndStoreContents(fileName, &ciphertext);
+                if (ciphertext == NULL)
+                    ciphertext = "1";
+
+                // Send the length of the incoming data.
+
+                messageLength = strlen(ciphertext);
+                memset(buffer, '\0', sizeof(buffer));
+                sprintf(buffer, "%d", messageLength);
+
+                // Send meta data. Length of the incoming ciphertext.
+                charsRead = send(establishedConnectionFD, buffer, strlen(buffer), 0);
+                if (charsRead < 0)
+                    error("ERROR writing metadata to socket");
+                // Send the string back.
+                charsRead = sendAll(establishedConnectionFD, ciphertext); // Send success back
+                if (charsRead < 0)
+                    error("ERROR writing to socket");
+
+                // Free memory.
+                if (ciphertext != NULL && ciphertext != "1")
+                    free(ciphertext);
+
+                // Delete the file.
+                if (fileName != NULL)
+                    remove(fileName);
+            }
+
+            // Send a Success message back to the client
+            close(establishedConnectionFD); // Close the existing socket which is connected to the client
+            exit(0);
+        }
+        close(establishedConnectionFD); // Close the existing socket which is connected to the client
+    }
+    // Else the process is the parent process.
+    // The parent process continues listening for incoming connections.
+    close(listenSocketFD); // Close the listening socket
+    return 0;
+}
